c5008d5d8bcf98757481b32fca8eae684abc31ae
jenkinsctsdemo <vinay.nagaraj@cognizant.com>
2014-11-12 10:56:53 +0000

    source code

---
+++ src/Demo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Demo
+{
+    public class Program
+    {
+        //TODO: document this method
+        private static void Main(string[] args)
+        {
+            //hack: we really should not be doing this!
+            //fixme: lets find a better way of doing this.
+            Console.Out.WriteLine("Hello Jenkins!");
+        }
+
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static int Subtract(int x, int y)
+        {
+            return x - y;
+        }
+
+        public static int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+
+        public static int Divide(int x, int y)
+        {
+            return x / y;
+        }
+    }
+}
---
+++ src/DemoTests/ProgramTests.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using Demo;
+
+namespace DemoTests
+{
+  [TestFixture]
+  public class ProgramTests
+  {
+    [Test]
+    public void Add()
+    {
+      Assert.AreEqual(42, Program.Add(40, 2));
+    }
+
+    [Test]
+    public void Subtract()
+    {
+        Assert.AreEqual(1066, Program.Subtract(1100, 34));
+    }
+
+    [Test]
+    public void Multiply()
+    {
+        Assert.AreEqual(100, Program.Multiply(10, 10));
+    }
+
+    [Test]
+    public void Divide()
+    {
+        Assert.AreEqual(10, Program.Divide(100, 10));
+    }
+  }
+}
