6371563e9f3d06ffb1d397cfe32147d4d69ac9b9
jenkinsctsdemo <vinay.nagaraj@cognizant.com>
2015-10-21 03:44:22 -0500

    Update postshell.sh

--- postshell.sh
+++ postshell.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 cp /etc/hosts /etc/hosts.bak
-cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
 echo '10.253.245.154 svnsonarbkup.metlife.com svnsonarbkup' >> /etc/hosts
 echo ClientAliveInterval 60 >> /etc/ssh/sshd_config
 echo ClientAliveCountMax 60 >> /etc/ssh/sshd_config
